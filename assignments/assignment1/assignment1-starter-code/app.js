bcb31c9b45c8ba02f264f2f32e5d48feab60ba24
gitmat <fmjetable-dev@yahoo.com>
2017-01-09 20:06:00 -0500

    Update app.js
    another update

--- assignments/assignment1/assignment1-starter-code/app.js
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -9,7 +9,7 @@ $scope.lunchitems = "";
 function LunchCheckController($scope) {
 	$scope.CheckLunchItems = function() {
 		var lunch_items = $scope.lunchitems.split(',');
-		if(lunch_items.length > 0) {
+		if(lunch_items != '') {
 			$scope.message = ((lunch_items.length < 4) ? "Enjoy!" : "Too many items");
 		} else {
 			$scope.message = "Please enter data first";
